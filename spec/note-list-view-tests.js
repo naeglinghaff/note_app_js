ac414705cc694c56b421ebd7dcf24df565ea670e
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-31 14:38:45 +0100

    Error for no notes

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -7,4 +7,20 @@ describe('Notelistview', function () {
     notelist.addNote(note);
     assert.isTrue(notelistview.viewNotesAsHTML() === '<ul><li><div>hello</div></li></ul>');
   });
+
+  describe('it can list 2 notes', function testForPrintingNotes() {
+    let notelist = new Notelist;
+    let notelistview = new Notelistview(notelist);
+    let note = new Note("hello");
+    let note2 = new Note("test 2");
+    notelist.addNote(note);
+    notelist.addNote(note2);
+    assert.isTrue(notelistview.viewNotesAsHTML() === '<ul><li><div>hello</div></li><li><div>test 2</div></li></ul>');
+  });
+
+  describe('it can throws error for empty notelist', function testForPrintingNotes() {
+    let notelist = new Notelist;
+    let notelistview = new Notelistview(notelist);
+    assert.isTrue(notelistview.viewNotesAsHTML() === 'No notes');
+  });
 });
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -5,11 +5,15 @@
    }
 
    viewNotesAsHTML() {
-     let x =  this.notelist.notes.map(note => {
-         return (`<li><div>${note.text}</div></li>`);
-     });
+     if (this.notelist.notes.length === 0) {
+        throw new Error("No notes");
+     } else {
+       let x =  this.notelist.notes.map(note => {
+          return (`<li><div>${note.text}</div></li>`);
+        });
 
-     x = `<ul>${x.join('')}</ul>`;
-     return x;
+        x = `<ul>${x.join('')}</ul>`;
+        return x;
+    }
    }
 }
