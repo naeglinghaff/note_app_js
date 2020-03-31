f50b4579bc4ea067f258ff4c850cc92536f85a91
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-31 10:29:58 +0100

    Passed html list test

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -5,6 +5,17 @@ describe('Notelistview', function () {
     let notelistview = new Notelistview(notelist);
     let note = new Note("hello");
     notelist.addNote(note);
-    assert.isTrue(notelistview.viewNotesAsHTML() === note);
+    assert.isTrue(notelistview.viewNotesAsHTML() === '<li>hello</li>');
   });
 });
+
+
+// function noteListViewCreatesHTMLList() {
+//     console.log("noteListViewCreatesHTMLList");
+//     var noteList = new NoteList();
+//     var noteListView = new NoteListView(noteList);
+//     noteList.createNote("test 1");
+//     noteList.createNote("test 2");
+//     assert.isTrue(noteListView.listHTML() === '<ul><li><div><a href=#notes/0>test 1</a></div></li></ul><ul><li><div><a href=#notes/1>test 2</a></div></li></ul>');
+//
+// }
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,8 +3,16 @@ function Notelistview(notelist) {
 }
 
 Notelistview.prototype.viewNotesAsHTML = function() {
-  let list = "<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>"
-    let x =  this.notelist.map(note => {
-      return (`<div>${note.text}</div>`);
-      });
+  let x =  this.notelist.notes.map(note => {
+      return (`<li>${note.text}</li>`);
+  });
+
+  x = x.join("");
+  return x;
   };
+
+notelist = new Notelist;
+note = new Note("hello");
+notelist.addNote(note);
+notelistview = new Notelistview(notelist);
+notelistview.viewNotesAsHTML();
